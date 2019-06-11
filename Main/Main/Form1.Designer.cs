b2d9b8c3d2fd6d7da079736a705a0324baf2bc26
Kasug <49664593+Kasugalex@users.noreply.github.com>
2019-06-11 19:16:24 +0800

    Update

--- Main/Main/Form1.Designer.cs
+++ Main/Main/Form1.Designer.cs
@@ -28,13 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Close = new System.Windows.Forms.PictureBox();
+            this.Start = new System.Windows.Forms.Label();
+            this.End = new System.Windows.Forms.Label();
+            this.StartPath = new System.Windows.Forms.TextBox();
+            this.endPath = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Close)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Close
+            // 
+            this.Close.BackColor = System.Drawing.Color.Transparent;
+            this.Close.BackgroundImage = global::Main.Properties.Resources.Close;
+            this.Close.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Close.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Close.Location = new System.Drawing.Point(452, 12);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(36, 32);
+            this.Close.TabIndex = 0;
+            this.Close.TabStop = false;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // Start
+            // 
+            this.Start.Font = new System.Drawing.Font("微软雅黑", 14F);
+            this.Start.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Start.Location = new System.Drawing.Point(27, 78);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(60, 23);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "Start:";
+            this.Start.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // End
+            // 
+            this.End.Font = new System.Drawing.Font("微软雅黑", 14F);
+            this.End.Location = new System.Drawing.Point(32, 130);
+            this.End.Name = "End";
+            this.End.Size = new System.Drawing.Size(55, 25);
+            this.End.TabIndex = 2;
+            this.End.Text = "End:";
+            this.End.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // StartPath
+            // 
+            this.StartPath.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.StartPath.Font = new System.Drawing.Font("微软雅黑", 13F);
+            this.StartPath.Location = new System.Drawing.Point(93, 80);
+            this.StartPath.Name = "StartPath";
+            this.StartPath.Size = new System.Drawing.Size(293, 30);
+            this.StartPath.TabIndex = 3;
+            // 
+            // endPath
+            // 
+            this.endPath.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.endPath.Font = new System.Drawing.Font("微软雅黑", 13F);
+            this.endPath.Location = new System.Drawing.Point(93, 130);
+            this.endPath.Name = "endPath";
+            this.endPath.Size = new System.Drawing.Size(293, 30);
+            this.endPath.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::Main.Properties.Resources.Add;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Location = new System.Drawing.Point(392, 78);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(24, 24);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.AddStart_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = global::Main.Properties.Resources.Add;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Location = new System.Drawing.Point(392, 131);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(24, 24);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.AddEnd_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 18F);
+            this.button1.Location = new System.Drawing.Point(139, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 56);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Do♂it";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Doit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(500, 250);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.endPath);
+            this.Controls.Add(this.StartPath);
+            this.Controls.Add(this.End);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.Close);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Junction";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Close)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox Close;
+        private System.Windows.Forms.Label Start;
+        private System.Windows.Forms.Label End;
+        private System.Windows.Forms.TextBox StartPath;
+        private System.Windows.Forms.TextBox endPath;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Main/Main/Form1.cs
+++ Main/Main/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -11,10 +12,66 @@ using System.Windows.Forms;
 namespace Main
 {
     public partial class Form1 : Form
-    {
+    { 
+        
+        //定义无边框窗体Form
+        [DllImport("user32.dll")]//*********************拖动无窗体的控件
+        public static extern bool ReleaseCapture();
+        [DllImport("user32.dll")]
+        public static extern bool SendMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
+        public const int WM_SYSCOMMAND = 0x0112;
+        public const int SC_MOVE = 0xF010;
+        public const int HTCAPTION = 0x0002;
+
+        private void gPanelTitleBack_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, WM_SYSCOMMAND, SC_MOVE + HTCAPTION, 0);//*********************调用移动无窗体控件函数
+        }
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.MouseDown += new MouseEventHandler(this.gPanelTitleBack_MouseDown);
+        }
+
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        private void Doit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddStart_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择路径";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                StartPath.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void AddEnd_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择路径";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                endPath.Text = dialog.SelectedPath;
+            }
+        }
+
     }
 }
